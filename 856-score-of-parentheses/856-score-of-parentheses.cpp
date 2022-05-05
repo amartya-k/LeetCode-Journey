60e2a7a9cfd7dcb1d700c2cafc9885072347bff0
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-05-05 21:39:36 +0530

    Time: 3 ms (29.06%), Space: 5.9 MB (97.07%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    int scoreOfParentheses(string str) {
-        stack<int>s;
-        s.push(0);
-        for(auto c:str){
-            if(c=='(')
-                s.push(0);
-            else{
-                int t=s.top();
-                s.pop();
-                if(t==0){
-                    s.top()+=1;
+    // int scoreOfParentheses(string str) {
+    //     stack<int>s;
+    //     s.push(0);
+    //     for(auto c:str){
+    //         if(c=='(')
+    //             s.push(0);
+    //         else{
+    //             int t=s.top();
+    //             s.pop();
+    //             if(t==0){
+    //                 s.top()+=1;
+    //             }
+    //             else{
+    //                 s.top()+=(2*t);
+    //             }
+    //         }
+    //     }
+    //     return s.top();
+    // }
+        int scoreOfParentheses(string str) {
+            int depth=0,sc=0,prev;
+            for(auto c:str){
+                if(c=='('){
+                    depth++;
                 }
                 else{
-                    s.top()+=(2*t);
+                    depth--;
+                    if(prev=='(')
+                        sc+=1<<depth;
                 }
+                prev=c;
             }
+        return sc;
+        
         }
-        return s.top();
-    }
+
 };
