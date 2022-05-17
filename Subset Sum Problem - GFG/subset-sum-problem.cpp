eb2e86e207c2e5bf1c4c94a754d176270e13cc36
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-05-17 11:12:19 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,56 +9,56 @@ using namespace std;
 
 class Solution{   
 public:
-    vector<vector<bool>>dp;
+    vector<vector<int>>dp;
     Solution(){
-        dp=vector<vector<bool>>(102,vector<bool>(100001,false));
+        dp=vector<vector<int>>(102,vector<int>(100001,-1));
     }
     bool isSubsetSum(vector<int>a, int sum){
-        // code here 
-        //return isSubset(arr,sum,arr.size());
-        for(int i=0;i<=a.size();i++){
-            dp[i][0]=true;
-        }
+    //     // code here 
+        return isSubset(a,sum,a.size());
+    //     for(int i=0;i<=a.size();i++){
+    //         dp[i][0]=true;
+    //     }
         
-        for(int i=1;i<=sum;i++){
-            dp[0][i]=false;
-        }
+    //     for(int i=1;i<=sum;i++){
+    //         dp[0][i]=false;
+    //     }
         
-        //Main logic
-        for(int i=1;i<=a.size();i++){
-            for(int j=1;j<=sum;j++){
-                if(a[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[a.size()][sum];
+    //     //Main logic
+    //     for(int i=1;i<=a.size();i++){
+    //         for(int j=1;j<=sum;j++){
+    //             if(a[i-1]<=j){
+    //                 dp[i][j]=dp[i-1][j-a[i-1]] || dp[i-1][j];
+    //             }
+    //             else{
+    //                 dp[i][j]=dp[i-1][j];
+    //             }
+    //         }
+    //     }
+    //     return dp[a.size()][sum];
     }
     
-    // bool isSubset(vector<int>a, int sum,int n){
-    //     // code here 
-    //     //Base
-    //     if(sum==0)
-    //     return true;
-    //     else if(n<=0)
-    //     return false;
+    bool isSubset(vector<int>a, int sum,int n){
+        // code here 
+        //Base
+        if(sum==0)
+        return 1;
+        else if(n<=0)
+        return 0;
         
-    //     if(dp[n-1][sum]){
-    //         return dp[n-1][sum];
-    //     }
+        if(dp[n-1][sum]!=-1){
+            return dp[n-1][sum];
+        }
         
-    //     if(a[n-1]<=sum){
-    //         return dp[n-1][sum] = isSubset(a,sum-a[n-1],n-1) || isSubset(a,sum,n-1);
+        if(a[n-1]<=sum){
+            return dp[n-1][sum] = isSubset(a,sum-a[n-1],n-1) || isSubset(a,sum,n-1);
             
-    //     }
-    //     else{
-    //         return dp[n-1][sum] = isSubset(a,sum,n-1);
-    //     }
+        }
+        else{
+            return dp[n-1][sum] = isSubset(a,sum,n-1);
+        }
         
-    // }
+    }
 };
 
 // { Driver Code Starts.
