f3956a8fc3f4ba65281915c800b5e696cfd05022
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-05-21 21:00:11 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -11,30 +11,37 @@ class Solution{
 	    int total=0;
 	    for(int i=0;i<n;i++)
 	        total+=a[i];
+	    //cout<<total<<endl;
+	    //vector<vector<bool>>dp(n+1, vector<bool>(total+1,false));
+	    int mid=(total/2)+1;
+	    vector<bool>dp1(mid,false);
+	    vector<bool>dp2(mid,false);
 	    
-	    vector<vector<bool>>dp(n+1, vector<bool>(total+1,false));
-	    
-	    for(int i=0;i<dp.size();i++){
-	       dp[i][0]=true;
-	    }
+	    dp1[0]=true;
+	    dp2[0]=true;
 	    
 	    for(int i=1;i<n+1;i++){
-	        for(int j=1;j<total+1;j++){
+	        for(int j=1;j<mid;j++){
 	            if(a[i-1]<=j){
-	                dp[i][j]= dp[i-1][j] || dp[i-1][j-a[i-1]];
+	                //dp[i][j]= dp[i-1][j] || dp[i-1][j-a[i-1]];
+	                dp2[j] = dp1[j] || dp1[j-a[i-1]];
 	            }
 	            else{
-	                dp[i][j]=dp[i-1][j];
+	                dp2[j]=dp1[j];
 	            }
+	           
 	        }
+	         dp1=dp2;
 	    }
+	    
 	   
 	    int mn=INT_MAX;
-	    for(int i=total/2;i>=0;i--){
-	        if(dp[n][i]){
+	    for(int i=dp2.size()-1;i>=0;i--){
+	        if(dp2[i]){
 	            //cout<<i<<endl;
 	            mn=min(mn,abs(total-2*i));
 	            break;
+	            
 	        }
 	    }
 	    
