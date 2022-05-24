d77a037a3acdb6e79207708608f9171c6eec3463
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-05-24 21:21:53 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -9,24 +9,33 @@ class Solution {
 
         // code here.
         
-        vector<vector<long long int>>dp(tcoins+1,vector<long long int>(w+1,0));
-        for(int i=0;i<tcoins+1;i++){
-            dp[i][0]=1;
-        }
+        // vector<vector<long long int>>dp(tcoins+1,vector<long long int>(w+1,0));
+        // for(int i=0;i<tcoins+1;i++){
+        //     dp[i][0]=1;
+        // }
         
-        //main logic
-        for(int i=1;i<tcoins+1;i++){
-            for(int j=0;j<w+1;j++){
-                if(j>=S[i-1]){
-                    dp[i][j]= dp[i-1][j] + dp[i][j-S[i-1]];
+        // //main logic
+        // for(int i=1;i<tcoins+1;i++){
+        //     for(int j=0;j<w+1;j++){
+        //         if(j>=S[i-1]){
+        //             dp[i][j]= dp[i-1][j] + dp[i][j-S[i-1]];
                     
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[tcoins][w];
+        vector<long long int>dp(w+1,0);
+        dp[0]=1;
+        
+        for(int i=0;i<tcoins;i++){
+            for(int j=S[i];j<=w;j++){
+                dp[j]+=dp[j-S[i]];
             }
         }
-        return dp[tcoins][w];
+        return dp[w];
     }
 };
 
