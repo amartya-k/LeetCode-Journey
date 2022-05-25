506b2f3e2d8cfeb3c5a0ee2304781ed99021091a
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-05-25 13:07:07 +0530

    Time: 204 ms (17.38%), Space: 14.5 MB (34.57%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& cn, int V) {
-        int n=cn.size();
-        vector<vector<int>>dp(n+1,vector<int>(V+1,0));
-        for(int i=1;i<V+1;i++){
-            dp[0][i]=V+1;
-        }
+        vector<int>dp(V+1,V+1);
+        dp[0]=0;
         
-        //main logic
-        for(int i=1;i<n+1;i++){
-            for(int j=0;j<V+1;j++){
-                if(j>=cn[i-1]){
-                    dp[i][j]= min(dp[i-1][j], 1+ dp[i][j-cn[i-1]]);
-                    
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+        for(int i=1;i<V+1;i++){
+            for(int j=0;j<cn.size();j++){
+                if(i>=cn[j])
+                    dp[i]=min(dp[i],1+dp[i-cn[j]]);
             }
         }
-        
-        
-        return dp[n][V]==V+1?-1:dp[n][V];
+        return dp[V]==V+1?-1:dp[V];
     }
 };
