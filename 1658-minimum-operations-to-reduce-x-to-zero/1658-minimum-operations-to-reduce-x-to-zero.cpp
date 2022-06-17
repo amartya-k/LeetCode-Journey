92cac24112cf22c6446c712ecc028d856a976c3c
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-06-17 20:17:35 +0530

    Time: 236 ms (72.94%), Space: 98.5 MB (84.14%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int sum = 0, n = nums.size();
-        for (int i : nums) sum += i;
-        int target = sum - x;
-        int curr_sum = 0, max_len = 0;
-        int start_idx = 0;
-        bool found = false;
-        for (int end_idx = 0; end_idx < n; end_idx++) {
-            curr_sum += nums[end_idx];
-            while (start_idx <= end_idx && curr_sum > target) {
-                curr_sum -= nums[start_idx];
-                start_idx += 1;
-            }
-            if (curr_sum == target) {
-                found = true;
-                max_len = max(max_len, end_idx - start_idx + 1);
-            }
+        if(nums.size()==1 && nums[0]==x)
+            return 1;
+        int sum=0, mx=INT_MIN,t_sum=0,k=0,i=0,j=0;
+        
+        for(int i=0;i<nums.size();i++){
+            t_sum+=nums[i];
         }
-        return found ? n - max_len : -1;
+        //cout<<"total="<<t_sum<<endl;
+        //sum of the maximum subarray
+        k=t_sum-x;
+        if(k<0)
+            return -1;
+        //apply sliding window 
+        while(j<nums.size()){
+            sum+=nums[j];
+            while(i<nums.size() && sum>k){
+                sum-=nums[i++]; //slide the window
+            }
+            if(sum==k){
+                //cout<<"i,j="<<i<<", "<<j<<endl;
+                mx=max(mx,j-i+1);
+            }
+            j++;
+        }
+        return mx==INT_MIN?-1:nums.size()-mx;
     }
 };
-/*
-[1,1,4,2,3] 
-max_sum=11, update-> max_sum-=x or 11-5=6 
-in maximum subarray->take mx no. of elements
-Kadane's algo:
-csum+=v[i];
-m=max(m,csum);
-csum= csum<0?0:csum;
-
-
-*/
