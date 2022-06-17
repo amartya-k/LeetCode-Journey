edbf64d8344b8b9a241094b1f748b8a9de1904b2
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-06-17 20:22:54 +0530

    Time: 210 ms (81.12%), Space: 98.4 MB (97.84%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        if(nums.size()==1 && nums[0]==x)
-            return 1;
+        // if(nums.size()==1 && nums[0]==x)
+        //     return 1;
         int sum=0, mx=INT_MIN,t_sum=0,k=0,i=0,j=0;
         
         for(int i=0;i<nums.size();i++){
@@ -11,12 +11,12 @@ public:
         //cout<<"total="<<t_sum<<endl;
         //sum of the maximum subarray
         k=t_sum-x;
-        if(k<0)
-            return -1;
+        // if(k<0)
+        //     return -1;
         //apply sliding window 
         while(j<nums.size()){
             sum+=nums[j];
-            while(i<nums.size() && sum>k){
+            while(i<=j && sum>k){
                 sum-=nums[i++]; //slide the window
             }
             if(sum==k){
