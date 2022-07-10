7a18e821e27067c80dfe9d20155cca88c7fac896
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-07-10 22:05:16 +0530

    Time: 11 ms (38.17%), Space: 7.7 MB (87.80%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    int ham(int n){
-        int cnt=0;
-        while(n){
-            cnt++;
-            n=n&(n-1);
-        }
-        return cnt;
-    }
     vector<int> countBits(int n) {
         vector<int>v(n+1);
+        v[0]=0;
         
-        for(int i=0;i<n+1;i++){
-            v[i]=ham(i);
+        for(int i=1;i<n+1;i++){
+            v[i]=v[i>>1]+(i&1);
         }
         return v;
     }
