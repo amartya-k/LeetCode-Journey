41449e1ab6b037b558280c13b87633c96b97400b
Amartya Kalapahar <amartyaandroid@gmail.com>
2022-07-11 21:57:18 +0530

    Time: 45 ms (13.20%), Space: 18 MB (63.93%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& v) {
-        int n=v.size();
-        int sum = n*(n+1)/2;
-        int sum1 = accumulate(v.begin(), v.end(), 0);
-        return (sum-sum1);
+        // int n=v.size();
+        // int sum = n*(n+1)/2;
+        // int sum1 = accumulate(v.begin(), v.end(), 0);
+        // return (sum-sum1);
+        int a=v[0],b=0;
+        for(int i=1;i<v.size();i++){
+            a^=v[i];
+            b^=i;
+        }
+        b^=(v.size());
+        return a^b;
     }
 };
